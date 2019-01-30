9e12723dcb5065cb88f5f7cc22736a15c78eed91
sylvain <sylvain@stilde.fr>
2019-01-30 19:03:15 +0100

    fourth commit

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:appjson/models/image_model.dart';
+import 'dart:math';
 
 class ImageList extends StatelessWidget {
   final List<ImageModel> images;
@@ -11,8 +12,28 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
+        return buildImage(images[index]);
       },
     );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          padding: EdgeInsets.all(20.0),
+          margin: EdgeInsets.all(20.0),
+          child: Transform(
+    alignment: Alignment.topRight,
+    transform: Matrix4.skewY(0.3)..rotateZ(-pi / 12.0),
+    child: Container(
+      padding: const EdgeInsets.all(8.0),
+      color: const Color(0xFFE8581C),
+      child:  Image.network(image.url),
+    ),
+          ),    
+         
+        );
   }
 }
